7a67d7e4e5850901608e8cc8f5cf02f453c1db1e
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-11 23:04:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,7 @@ def monopoly_with_fourth_tier
 		b_and_o_railroad:
 		{"mortgage_value" => "$400"},
 		shortline:
-		{"mortgage_value" => "$300"}
+		{"mortgage_value" => "$800"}
 		},
 		rent_in_dollars:
 		{ one_piece_owned: 25,
